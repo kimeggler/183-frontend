c0c53ec9edcff7b69c77a03ac9f77d1945993a17
Kim <kim.eggler@gmail.com>
2021-01-13 09:59:03 +0100

    base setup

---
+++ src/config/config.js
@@ -0,0 +1,10 @@
+const config = {
+  environment: 'production',
+  remoteUrl: 'http://localhost:4000/',
+  authParams: {
+    client_id: 'f09561735a2044789f11c9bbfd814238',
+    redirect_uri: `${origin}/callback`,
+  },
+};
+
+export default config;
